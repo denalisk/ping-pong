e6c2e56675f99f173f86069e468b2d900fc5e622
Sam Kirsch <samdkirsch@gmail.com>
2017-01-20 09:22:18 -0800

    collect all functions under the pingPong function and move integer checking functionality to front end. Also will add an error div in the HTML to inform the user if they input something invalid

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,48 @@
 var firstCount = true;
 
-var makeElement = function(aString, element, parentId, elementId) {
-  var newElement = document.createElement(element);
-  newElement.id = elementId;
-  newElement.innerHTML = aString;
-  document.getElementById(parentId).appendChild(newElement);
-};
+var pingPong = function(number) {
+  // the overall function will count up to a number, replacing 3 with ping, 5 with pong, and 15 with ping-pong
 
-var countUp = function(number) {
-  for (index = 1; index <= number; index++) {
-    if (index % 15 === 0) {
-      makeElement("ping-pong", "li", "results");
-    } else if (index % 3 === 0) {
-      makeElement("ping", "li", "results");
-    } else if (index % 5 === 0) {
-      makeElement("pong", "li", "results");
-    } else {
-      makeElement(index, "li", "results");
+  var makeElement = function(aString, element, parentId, elementId) {
+    // this function creates new elements in the HTML document
+    var newElement = document.createElement(element);
+    newElement.id = elementId;
+    newElement.innerHTML = aString;
+    document.getElementById(parentId).appendChild(newElement);
+  };
+
+  var countUp = function(number) {
+    // this function takes a numbers and creates a list element to hold the number itself or a ping/pong placeholder
+    for (index = 1; index <= number; index++) {
+      if (index % 15 === 0) {
+        makeElement("ping-pong", "li", "results");
+      } else if (index % 3 === 0) {
+        makeElement("ping", "li", "results");
+      } else if (index % 5 === 0) {
+        makeElement("pong", "li", "results");
+      } else {
+        makeElement(index, "li", "results");
+      };
+    };
+    firstCount = false;
+  };
+
+  var resetCount = function() {
+    // this function resets the count if it already exists
+    if (firstCount != true) {
+      var oldList = document.getElementById("results");
+      oldList.parentNode.removeChild(oldList);
+      makeElement("", "ul", "list-holder", "results")
     };
   };
-  firstCount = false;
-};
 
-var resetCount = function() {
-  if (firstCount != true) {
-    var oldList = document.getElementById("results");
-    oldList.parentNode.removeChild(oldList);
-    makeElement("", "ul", "list-holder", "results")
-  };
-};
-
-var pingPong = function(number) {
+  // this is the meat of the main function
   if (/[^\d]/.test(number) === true) {
     return "Please input a whole integer, no decimals";
   } else {
     resetCount();
     countUp(number);
-
+  };
 }
 
 
@@ -45,11 +51,11 @@ $(function(){
   $(".count-form").submit(function(event) {
     event.preventDefault();
     var number = $("#number-input").val();
-    if (firstCount != true) {
-      resetCount();
-      countUp(number);
+    if (/[^\d]/.test(number) === true) {
+      $("#error").text("Please input a whole integer, no decimals");
     } else {
-      countUp(number);
+      $("#error").text("...");
+      pingPong(number);
     }
   });
 });
